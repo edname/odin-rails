61c3936ee34cc9c027875b234cabbedbd059498e
Edward <lt93970@gmail.com>
2022-01-12 17:42:08 +0200

    Project change to demo blog app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root 'cars#index'
-  resources :cars
+  root 'articles#index'
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+
+  get "/shop", to: "product#index"
+  get "/shop/:id", to: "product#show"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_141710) do
+ActiveRecord::Schema.define(version: 2022_01_12_150906) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
     t.integer "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
