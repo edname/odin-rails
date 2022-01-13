d5e3b87b76b720aab2acedeb423b662355cdd2fd
Edward <lt93970@gmail.com>
2022-01-13 10:16:40 +0200

    Articles CRUD feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
 
-  get "/shop", to: "product#index"
-  get "/shop/:id", to: "product#show"
+  # get "/shop", to: "product#index"
+  # get "/shop/:id", to: "product#show"
+
+  resources :articles
+  resources :shop, path: 'product'
 
 end
